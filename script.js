b0bc6a54582b232a5c89329dc668a73355838098
Holly Henske <hcover333@gmail.com>
2022-04-22 18:40:43 -0500

    different video same problem.  My coding doesn't behave like the one in the video

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-let email = document.getElementById("email");
+"use strict"
+
+let email = document.getElementById("name.email");
 let dangerIcon = document.getElementById("danger-button");
 let dangerText = document.getElementById("danger-text");
 let form = document.getElementById("form");
 
-form.addEventListener("submit", (e) => {
-    if (email.validity.typeMismatch) {
-        email.preventDefault();
-        dangerIcon.style.opacity = 1;
-        dangerText.style.opacity = 1;
+console.log(email)
+
+form.addEventListener("submit", (email) => {
+    if (email.length > 7 ) {
+        dangerIcon.style.visibility = hidden;
+        dangerText.style.visibility = hidden;
+        console.log(email.length)
+        console.log("valid email")
     } else {
-        dangerIcon.style.opacity = 0;
-        dangerText.style.opacity = 0;
+        e.preventDefault();
+        // dangerIcon.style.visibility = true;
+        // dangerText.style.opacity = true; 
+        console.log(email.length)
+        console.log("invalid email")
     }
 });
+
+// && email.contains("@") && email.contains(".")
