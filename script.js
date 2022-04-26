83b329cafd5b480ef9d47a68ca3819dba8549738
Holly Henske <hcover333@gmail.com>
2022-04-26 08:41:56 -0500

    almost done, issue with js file and logic

--- script.js
+++ script.js
@@ -1,21 +1,89 @@
 
+// const btn = document.querySelector('button');
+// const form = document.getElementById('mail');
+// const mail = document.getElementById('email');
+// const list = document.getElementById('list');
+// const errMsg = document.querySelector('#errMsg');
 
-function checkEmail() {
-    let error_img = document.getElementById('error-img');
-    error_img.style.display = "none";
-    let error_info = document.getElementById('error-info');
-    error_info.style.display = "none";
+// const newMsg = () => {
+//     const img = document.createElement('img');
+//     img.crc = 'images/icon-error.svg'
+//     let msg = 'Please Enter a valid email';
+//     errMsg.append(img);
+//     errMsg.append(msg);
+//     list.append(errMsg);
+// }
 
-    let emailField = document.getElementById("email-field");
-    let email = emailField.value;
-    console.log(email);
-    
-    if (email.length > 7 && email.contains('@') && email.contains('.') ) {
-        emailField.style.border = '0.25px solid #f96262';
-        error_info.style.display = "block";
-        error_img.style.display="inline";
-    } else {
-        emailField.style.border = '1.25px solid #abd499';
-        
+// const mailValidation = () => {
+//     let email = mail.value;
+//     if (email > 7 && email.contains('@') && email.contains('.')) {
+//         console.log("valid email")
+//     } else {
+//         newMsg();
+//     }
+// }
+
+// const baseApparel = (e) => {
+//     e.preventDefault();
+//     mailValidation();
+//     btn.removeEventListener('click', baseApparel);
+// }
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+// })
+
+// mail.addEventListener('click', () => {
+//     errMsg.innerHTML = '';
+//     btn.addEventListener('click', baseApparel);
+// })
+
+// btn.addEventListener('click', baseApparel);
+
+const btn = document.querySelector('button');
+
+const form = document.querySelector('form');
+
+const mail = document.querySelector('input');
+
+const regx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+const list = document.querySelector('#list')
+
+const errMsg = document.querySelector('#errMsg')
+
+
+const newMsg = () => {
+    const img = document.createElement('img');
+    img.src = 'images/icon-error.svg'
+    let msg = 'Please Enter a valid email';
+    errMsg.append(img);
+    errMsg.append(msg);
+    list.append(errMsg);
+}
+
+const mailValidation = () => {
+    if (mail.value.match(regx)){
+        alert('you have entered a valid email')
+    }
+    else {
+        newMsg();
     }
 }
+
+const myFunction = (e)=> {
+    e.preventDefault();
+    mailValidation();
+    btn.removeEventListener('click', myFunction);
+   }
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+})
+
+mail.addEventListener('click', () => {
+    errMsg.innerHTML = '';
+    btn.addEventListener('click', myFunction);
+})
+
+btn.addEventListener('click', myFunction);
